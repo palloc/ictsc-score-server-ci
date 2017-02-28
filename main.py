a15887a14bf95e48fabb5de29089eb893a561ec3
palloc <palloc.mknet10@gmail.com>
2017-02-28 11:51:12 +0900

    s

--- main.py
+++ main.py
@@ -3,34 +3,25 @@
 import requests
 import json
 
-BASE_URL = 'http://stg.ictsc.pref.yokohama'
+# session
+sess = 'BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiRWY3MGIxODUzMjI2ZTdmZmJkMjYz%0AZjQzOTg0YzAyYWU3OTcwMjJiMDlhOTIzZTY5YzljNmYwMDllZmRmODI2YzMG%0AOwBGSSIObWVtYmVyX2lkBjsARmkW%0A--1500cac8d9f5e1df7a480ea66117ed0c098ffe2d'
+cookie = {'rack.session': sess}
+
+
+BASE_URL = ''
+with open('url.txt', 'r') as f:
+    BASE_URL = f.readline()
+
 
 res = requests.get(BASE_URL+'/api/teams')
 assert res.status_code == 200
 assert json.loads(res.text)
-
-res = requests.post(BASE_URL+'/api/teams')
-assert res.status_code == 403
 
 
 res = requests.get(BASE_URL+'/api/teams/1')
 assert res.status_code == 200
 assert json.loads(res.text)
 
-# 権限フィルタ
-res = requests.put(BASE_URL+'/api/teams/1')
-assert res.status_code == 404
-
-res = requests.patch(BASE_URL+'/api/teams/1')
-assert res.status_code == 404
-
-res = requests.delete(BASE_URL+'/api/teams/1')
-assert res.status_code == 404
-
-
-# session
-sess = 'BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiRWY3MGIxODUzMjI2ZTdmZmJkMjYz%0AZjQzOTg0YzAyYWU3OTcwMjJiMDlhOTIzZTY5YzljNmYwMDllZmRmODI2YzMG%0AOwBGSSIObWVtYmVyX2lkBjsARmkW%0A--1500cac8d9f5e1df7a480ea66117ed0c098ffe2d'
-cookie = {'rack.session': sess}
 
 res = requests.get(BASE_URL+'/api/answers', cookies = cookie)
 assert res.status_code == 200
@@ -55,3 +46,4 @@ assert json.loads(res.text)
 res = requests.get(BASE_URL+'/api/issues/comments/276000', cookies = cookie)
 assert res.status_code == 200
 assert json.loads(res.text)
+
