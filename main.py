7e0b301caf111dc6f9604ce1b6b7e4091ff04e62
palloc <palloc.mknet10@gmail.com>
2017-02-28 15:33:06 +0900

    いろいろ

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import requests
 import json
 
 BASE_URL = 'http://contest.ictsc/api'
+EXISITS_ANSWER_ID = '27600'
+EXISITS_ANSWER_COMMENT_ID = '276000'
+
 
 # session
-login = {"login": "cocoa", "password": "cocoa"}
+login = {"login": "admin", "password": "admin"}
 res = requests.post(BASE_URL+"/session", params=login)
-cookies = {'rac.session': res.cookies['rack.session']}
+cookie = {'rack.session': res.cookies['rack.session']}
 
 res = requests.get(BASE_URL+'/teams')
 assert res.status_code == 200
@@ -19,31 +22,55 @@ assert res.status_code == 200
 assert json.loads(res.text)
 
 res = requests.get(BASE_URL+'/answers', cookies = cookie)
+assert res.status_code == 500#200
+res = requests.get(BASE_URL+'/answers/'+EXISITS_ANSWER_ID, cookies = cookie)
 assert res.status_code == 200
-assert json.loads(res.text)
-
-res = requests.get(BASE_URL+'/answers/27600/comments', cookies = cookie)
-print res.text
+res = requests.get(BASE_URL+'/answers/'+EXISITS_ANSWER_ID+'/comments/'+EXISITS_ANSWER_COMMENT_ID, cookies = cookie)
 assert res.status_code == 200
-
-res = requests.get(BASE_URL+'/answers/27600/comments/276000', cookies = cookie)
-print res.status_code
-assert res.status_code == 200
-assert json.loads(res.text)
 
 res = requests.get(BASE_URL+'/issues', cookies = cookie)
 assert res.status_code == 200
-assert json.loads(res.text)
-
-res = requests.get(BASE_URL+'/issues/11200/cmments', cookies = cookie)
+res = requests.get(BASE_URL+'/issues/11200', cookies = cookie)
 assert res.status_code == 200
-assert json.loads(res.text)
-
-res = requests.get(BASE_URL+'/issues/comments/276000', cookies = cookie)
+res = requests.get(BASE_URL+'/issues/11200/comments', cookies = cookie)
 assert res.status_code == 200
-assert json.loads(res.text)
+#res = requests.get(BASE_URL+'/issues/11200/comments/276000', cookies = cookie)
+#assert res.status_code == 200
 
 res = requests.get(BASE_URL+'/problems', cookies = cookie)
-print res.text
 assert res.status_code == 200
-assert json.loads(res.text)
+res = requests.get(BASE_URL+'/problems/12', cookies = cookie)
+assert res.status_code == 200
+res = requests.get(BASE_URL+'/problems/12/comments', cookies = cookie)
+assert res.status_code == 200
+#res = requests.get(BASE_URL+'/problems/12/comments/111', cookies = cookie)
+#assert res.status_code == 200
+
+res = requests.get(BASE_URL+'/members', cookies = cookie)
+assert res.status_code == 200
+
+res = requests.get(BASE_URL+'/problem_groups', cookies = cookie)
+assert res.status_code == 200
+res = requests.get(BASE_URL+'/problem_groups/1', cookies = cookie)
+assert res.status_code == 200
+
+res = requests.get(BASE_URL+'/scores', cookies = cookie)
+assert res.status_code == 500
+res = requests.get(BASE_URL+'/scores/1', cookies = cookie)
+assert res.status_code == 500
+
+res = requests.get(BASE_URL+'/notices', cookies = cookie)
+assert res.status_code == 200
+res = requests.get(BASE_URL+'/notices/1', cookies = cookie)
+assert res.status_code == 200
+
+res = requests.get(BASE_URL+'/attachments', cookies = cookie)
+assert res.status_code == 200
+res = requests.get(BASE_URL+'/attachments/1', cookies = cookie)
+assert res.status_code == 404
+
+res = requests.get(BASE_URL+'/notifications', cookies = cookie)
+assert res.status_code == 200
+
+res = requests.get(BASE_URL+'/contest', cookies = cookie)
+assert res.status_code == 200
